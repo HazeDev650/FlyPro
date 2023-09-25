b8ba5bdadf992f6a1a615afc1e6ad06daf56bbef
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-09-24 23:37:33 -0700

    Update FlyCommand.php

--- src/Terpz710/FlyPlus/Commands/FlyCommand.php
+++ src/Terpz710/FlyPlus/Commands/FlyCommand.php
@@ -5,13 +5,24 @@ namespace Terpz710\FlyPlus\Commands;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use Terpz710\FlyPlus\Main;
+use pocketmine\utils\Config;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\Plugin;
 
 class FlyCommand extends Command {
 
-    public function __construct() {
+    private $config;
+    private $plugin;
+
+    public function __construct(Plugin $plugin, Config $config) {
         parent::__construct("fly", "Toggle flying");
+        $this->plugin = $plugin;
+        $this->config = $config;
         $this->setPermission("flyplus.fly");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
@@ -21,9 +32,6 @@ class FlyCommand extends Command {
             return true;
         }
 
-        $plugin = Main::getInstance();
-        $config = $plugin->getConfig();
-
         if (!$sender->hasPermission("flyplus.fly")) {
             $sender->sendMessage("You don't have permission to use this command.");
             return true;
@@ -32,22 +40,22 @@ class FlyCommand extends Command {
         if (empty($args)) {
             if ($sender->getAllowFlight()) {
                 $sender->setAllowFlight(false);
-                $sender->sendMessage($config->get("fly_message_off", "You have landed."));
+                $sender->sendMessage($this->config->get("fly_message_off", "You have landed."));
                 $this->sendFlyTitle($sender, "fly_title_off", "fly_subtitle_off");
             } else {
                 $sender->setAllowFlight(true);
-                $sender->sendMessage($config->get("fly_message_on", "You are now flying!"));
+                $sender->sendMessage($this->config->get("fly_message_on", "You are now flying!"));
                 $this->sendFlyTitle($sender, "fly_title_on", "fly_subtitle_on");
             }
         } elseif (count($args) === 1) {
             $subcommand = strtolower($args[0]);
             if ($subcommand === "on") {
                 $sender->setAllowFlight(true);
-                $sender->sendMessage($config->get("fly_message_on", "You are now flying!"));
+                $sender->sendMessage($this->config->get("fly_message_on", "You are now flying!"));
                 $this->sendFlyTitle($sender, "fly_title_on", "fly_subtitle_on");
             } elseif ($subcommand === "off") {
                 $sender->setAllowFlight(false);
-                $sender->sendMessage($config->get("fly_message_off", "You have landed."));
+                $sender->sendMessage($this->config->get("fly_message_off", "You have landed."));
                 $this->sendFlyTitle($sender, "fly_title_off", "fly_subtitle_off");
             } else {
                 $sender->sendMessage("Usage: /fly [on|off]");
@@ -60,12 +68,11 @@ class FlyCommand extends Command {
     }
 
     private function sendFlyTitle(Player $player, $titleKey, $subtitleKey) {
-        $config = Main::getInstance()->getConfig();
-        $title = $config->get($titleKey, "Fly Mode");
-        $subtitle = $config->get($subtitleKey, "Toggle your flight");
-        $fadeIn = $config->get("fly_title_fade_in", 10);
-        $stay = $config->get("fly_title_stay", 40);
-        $fadeOut = $config->get("fly_title_fade_out", 10);
+        $title = $this->config->get($titleKey, "Fly Mode");
+        $subtitle = $this->config->get($subtitleKey, "Toggle your flight");
+        $fadeIn = $this->config->get("fly_title_fade_in", 10);
+        $stay = $this->config->get("fly_title_stay", 40);
+        $fadeOut = $this->config->get("fly_title_fade_out", 10);
         $player->sendTitle($title, $subtitle, $fadeIn, $stay, $fadeOut);
     }
 }
